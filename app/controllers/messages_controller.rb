cac31c512cb9c0b6fa8bde4b44f4549c97fc5a36
Christopher Clark <chrisdclark3@gmail.com>
2015-07-17 23:15:18 -0700

    removed JSON.parse call and hoping for the best

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,29 +2,28 @@ class MessagesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    msg = JSON.parse(params)
     puts "IN CREATE METHOD"
     puts "PARAMS"
-    puts msg[:data]
+    puts params['data']
     puts "PARAMS"
-    puts msg[:data]
+    puts params['data']
     puts "PARAMS[sender_id]"
-    puts msg[:data][:sender_id]
+    puts params['data']['sender_id']
     puts "PARAMS[recipient_id]"
-    puts msg[:data][:recipient_id]
+    puts params['data']['recipient_id']
     puts "PARAMS[new_message]"
-    puts msg[:data][:new_message]
+    puts params['data']['new_message']
 
-    if Conversation.between(msg[:data][:sender_id], msg[:data][:recipient_id]).present?
-      @conversation = Conversation.between(msg[:data][:sender_id], msg[:data][:recipient_id]).first
+    if Conversation.between(params['data']['sender_id'], params['data']['recipient_id']).present?
+      @conversation = Conversation.between(params['data']['sender_id'], params['data']['recipient_id']).first
     else
-      @conversation = Conversation.create({ sender_id: msg[:data][:sender_id], recipient_id: msg[:data][:recipient_id] })
+      @conversation = Conversation.create({ sender_id: params['data']['sender_id'], recipient_id: params['data']['recipient_id'] })
     end
 
-    @message = @conversation.messages.build(body: msg[:data][:new_message])
-    @message.user_id = msg[:data][:sender_id]
+    @message = @conversation.messages.build(body: params['data']['new_message'])
+    @message.user_id = params['data']['sender_id']
     @message.save!
-    @conversations = Conversation.involving(User.find(msg[:data][:sender_id]))
+    @conversations = Conversation.involving(User.find(params['data']['sender_id']))
     if (@message.save)
       render json: @conversations.as_json(:include => [ :messages, :sender, :recipient ])
     else
