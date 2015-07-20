09fb1779398382790aabd44b1aab67002087706a
Christopher Clark <chrisdclark3@gmail.com>
2015-07-20 15:59:44 -0700

    verify authenticity skip added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   before_action :set_headers
 
   def set_headers
