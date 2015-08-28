213f55af3ece5304a4fa7b59e0d5afd2d7a63d62
Christopher Clark <chrisdclark3@gmail.com>
2015-08-28 00:22:30 -0700

    seeding without geocoding

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,8 @@ class User < ActiveRecord::Base
 		:path => ":class/:attachment/:id.:extension",
 	}
 
-	geocoded_by :address
-	after_validation :geocode
+	# geocoded_by :address
+	# after_validation :geocode
 
 	validates :avatar, :attachment_presence => true
   validates_attachment_content_type :avatar, :content_type => /\Aimage/
